9a2e3ae245124a5238f66b0f022fe8d227bac56b
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-10-03 13:35:51 -0400

    coded all

--- src/main/java/PhoneNumberGenerator.java
+++ src/main/java/PhoneNumberGenerator.java
@@ -1,19 +1,25 @@
 import java.util.Random;
+import java.text.DecimalFormat;
 
 public class PhoneNumberGenerator {
     public static void main(String[] args) {
         // TODO: Create Random object for phone number generation
-        
+        Random rand = new Random();
+        DecimalFormat df = new DecimalFormat("000");
+
         // TODO: Generate first three digits (XXX)
         // Constraint: Cannot contain 8 or 9 (digits 0-7 only)
-        
+        int dig1 = rand.nextInt(8);
+        int dig2 = rand.nextInt(8);
+        int dig3 = rand.nextInt(8);
         // TODO: Generate second three digits (XXX) 
         // Constraint: Must be <= 742 (000-742)
-        
+        int dig456 = rand.nextInt(743);
         // TODO: Generate third three digits (XXX)
         // No constraints (000-999)
-        
+        int dig789 = rand.nextInt(1000);
         // TODO: Format and display phone number as XXX-XXX-XXX
         // Include the dashes in the output
+        System.out.printf("%d%d%d-%s-%s", dig1, dig2, dig3, df.format(dig456), df.format(dig789));
     }
 }
--- src/main/java/RandomPercentage.java
+++ src/main/java/RandomPercentage.java
@@ -1,20 +1,28 @@
 import java.util.Random;
+import java.text.DecimalFormat;
 
 public class RandomPercentage {
     public static void main(String[] args) {
         // TODO: Create two Random objects
         // 1. One Random object without a seed (unseeded)
         // 2. One Random object with any seed value (seeded)
-        
+        Random seed = new Random(67);
+        Random unseed = new Random();
+        DecimalFormat df = new DecimalFormat("#.00%");
         // TODO: Generate random double values from both Random objects
         // Use nextDouble() method to get values between 0.0 and 1.0
-        
+        double unsint = unseed.nextDouble();
+        double sint = seed.nextDouble();
+
         // TODO: Format as percentages
         // Format the values to exactly 2 decimal places
         // Display with % symbol
-        
+        String unsstr = df.format(unsint);
+        String sstr = df.format(sint);
         // TODO: Display the values as percentages
         // Show both unseeded and seeded random values
         // Each value should be formatted as below
+        System.out.println("Unseeded Random: " + unsstr);
+        System.out.println("Seeded Random: " + sstr);
     }
 }
